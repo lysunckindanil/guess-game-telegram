9d36088eaf941e6a0ebfd0b0cfccf1cf5b21a9ba
Danila <danil.lysunckin@gmail.com>
2025-01-04 21:51:16 +0500

    token.properties

--- telegram-service/src/main/java/org/example/telegramservice/config/BotConfig.java
+++ telegram-service/src/main/java/org/example/telegramservice/config/BotConfig.java
@@ -3,9 +3,11 @@ package org.example.telegramservice.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
+@PropertySource("token.properties")
 public class BotConfig {
     @Value("${bot.token}")
     String token;
