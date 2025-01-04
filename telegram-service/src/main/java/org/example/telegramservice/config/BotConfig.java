e2d9e5550f675ce09f9187945fc2ace87a41fd21
Danila <danil.lysunckin@gmail.com>
2025-01-04 22:52:16 +0500

    PropertySource edit for maven build

--- telegram-service/src/main/java/org/example/telegramservice/config/BotConfig.java
+++ telegram-service/src/main/java/org/example/telegramservice/config/BotConfig.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
 @Data
-@PropertySource("token.properties")
+@Configuration
+@PropertySource("classpath:token.properties")
 public class BotConfig {
     @Value("${bot.token}")
     String token;
