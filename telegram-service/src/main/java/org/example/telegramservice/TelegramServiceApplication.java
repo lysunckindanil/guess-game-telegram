f34238f7729606455dc43855a7874a440752f25b
Danila <danil.lysunckin@gmail.com>
2025-01-04 15:34:28 +0500

    configuring eureka and telegram bot code review

--- telegram-service/src/main/java/org/example/telegramservice/TelegramServiceApplication.java
+++ telegram-service/src/main/java/org/example/telegramservice/TelegramServiceApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
